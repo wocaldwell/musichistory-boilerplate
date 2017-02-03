83d0b76245d22459e8d649ccc114e1977ddec2d2
William Caldwell <williamcaldwell@MacBook-Air.local>
2017-02-02 18:42:41 -0600

    Put songs in json file. Added delete button

--- js/songs.js
+++ js/songs.js
@@ -1,92 +1,41 @@
 "use strict";
 
-var songs = [
-"Legs > by Z*ZTop on the album Eliminator",
-"The Logical Song > by Supertr@amp on the album Breakfast in America",
-"Another Brick in the Wall > by Pink Floyd on the album The Wall",
-"Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction",
-"Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill"
-];
+var SongList = (function(songPopulator) {
+    var songs = [];
+    var mainContent = document.getElementById("selections");
 
-// ADDING A SONG STRING TO THE BEGGINING AND END OF ARRAY
-
-function addSomeSongs() {
-    songs.unshift("AFK > by Pinback on the album Summer in Abaddon");
-    songs.push("Lonesome > by Dr. Dog on the album Be the Void");
-    // console.log(songs);
-    return songs;
-}
-
-addSomeSongs();
-
-// REMOVING IRRERULAR CHARACTERS AND REPLACING ">" WITH "-"
-
-//Variable to store the edited songs
-var editedSongs = [];
-//Variable to store edited song string
-var charsOnlySong = "";
-//Upper and lower bounds for upper case characters
-var lowerBoundUpper = "A".charCodeAt(0);
-var upperBoundUpper = "Z".charCodeAt(0);
-//Upper and lower bounds for lower case characters
-var lowerBoundLower = "a".charCodeAt(0);
-var upperBoundLower = "z".charCodeAt(0);
-
-
-for (var i = 0; i < songs.length; i++) {
-    for (var j = 0; j < songs[i].length; j++) {
-        var currentSong = songs[i];
-        var charToAdd = currentSong[j];
-        var charToCheck = currentSong.charCodeAt(j);
-        if (charToCheck >= lowerBoundUpper && charToCheck <= upperBoundUpper) {
-            charsOnlySong += charToAdd;
-        } else if (charToCheck >= lowerBoundLower && charToCheck <= upperBoundLower) {
-            charsOnlySong += charToAdd;
-        } else if (charToAdd == " ") {
-            charsOnlySong += charToAdd;
-        } else if (charToAdd == "&") {
-            charsOnlySong += charToAdd;
-        } else if (charToAdd == ">") {
-            charsOnlySong += "-";
-        } else {
-            continue;
+    songPopulator.loadSongs = function(callback) {
+        var songLoader = new XMLHttpRequest();
+        songLoader.open("GET", "songs.json");
+        songLoader.send();
+        songLoader.addEventListener("error", function(event) {
+            console.log("There was an error loading");
+        });
+        songLoader.addEventListener("load", function(event) {
+            var songsData = JSON.parse(event.target.responseText);
+            for (var i in songsData) {
+                songs.push(songsData[i]);
+            }
+            callback(songsData);
+        });
+    };
+    songPopulator.populateSongs = function() {
+        for (var i = 0; i < songs.length; i++) {
+            var songsSong = songs[i];
+            mainContent.innerHTML += `  <div class='song'>
+                                            <h2>${songsSong.song}</h2>
+                                            <p>${songsSong.artist}</p>
+                                            <p>${songsSong.album}</p>
+                                            <p>${songsSong.genre}</p>
+                                            <button type="button" class="remove-button">Remove</button>
+                                        </div>`;
         }
-    }
-    editedSongs.push(charsOnlySong);
-    charsOnlySong = "";
-    // console.log(editedSongs);
-}
+    };
 
-// ADDING SONGS TO "MAIN CONTENT" DIV OF index.html
+    return songPopulator;
+})(SongList || {});
+SongList.loadSongs(SongList.populateSongs);
 
-//*********This technically is what the asignment called for*********
-
-// songs = editedSongs;
-// var mainContent = document.getElementById("selections");
-
-// for (var i = 0; i < songs.length; i++) {
-//     mainContent.innerHTML = mainContent.innerHTML + "<h2>" + songs[i] + "</h2>";
-// }
-
-//********But this made it formatted like the mock-up!!!************
-
-songs = editedSongs;
-var mainContent = document.getElementById("selections");
-
-function addAllSongs() {
-    for (var i = 0; i < songs.length; i++) {
-        var currentSong = songs[i];
-        var firstSplit = currentSong.split(" - by ");
-        var songName = firstSplit[0];
-        var firstRemainder = firstSplit[1];
-        var secondSplit = firstRemainder.split(" on the album ");
-        var artist = secondSplit[0];
-        var album = secondSplit[1];
-        mainContent.innerHTML = mainContent.innerHTML + "<div class='song'><h2>" + songName + "</h2><p>" + artist + "</p><p>" + album + "</p><p>Genre</p></div>";
-    }
-}
-
-addAllSongs();
 
 
 // Hide and show divs based on click
@@ -120,8 +69,8 @@ function addSongToSongs() {
     var artist = document.getElementById("addArtistName").value;
     var album =document.getElementById("addAlbumName").value;
     var songToAdd = song + " - by " + artist + " on the album " + album;
-    songs.push(songToAdd);
-    console.log(songs);
+    // songs.push(songToAdd);
+    // console.log(songs);
 }
 
 // add the class selected to nav links for correct syling
