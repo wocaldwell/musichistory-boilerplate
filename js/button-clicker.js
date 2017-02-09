8f6b7bafcfd3684e83a93587365ea056f4989af6
William Caldwell <williamcaldwell@MacBook-Air.local>
2017-02-09 16:58:12 -0600

    MVP for imlementing jquery

--- js/button-clicker.js
+++ js/button-clicker.js
@@ -1,16 +1,10 @@
-"use strict";
+// "use strict";
 
-var SongList = (function(buttonClicker) {
+// var SongList = (function(buttonClicker) {
 
-    buttonClicker.buttonClick = function() {
-        var moreButton = document.getElementById("more-button");
-        if (event.target.classList.contains("remove-button")) {
-            // Clicking on remove button removes song
-            event.target.parentElement.remove();
-        } if (event.target === moreButton) {
-            // Clicking on more button shows more songs
-            SongList.populateMoreSongs();
-        }
-    };
-    return buttonClicker;
-})(SongList || {});
+
+
+
+
+//     return buttonClicker;
+// })(SongList || {});
--- js/songs.js
+++ js/songs.js
@@ -1,79 +1,91 @@
 "use strict";
 
 var SongList = (function(songPopulator) {
-    var mainContent = document.getElementById("selections");
     var songs = [];
     var moreSongs = [];
-    songPopulator.loadSongs = function(callback) {
+    songPopulator.loadSongs = function() {
         // load initial songs to songs array
-        var songLoader = new XMLHttpRequest();
-        songLoader.open("GET", "songs.json");
-        songLoader.send();
-        songLoader.addEventListener("error", function(event) {
-            console.log("There was an error loading songs.json");
-        });
-        songLoader.addEventListener("load", function(event) {
-            var songsData = JSON.parse(event.target.responseText);
-            for (var i in songsData) {
-                songs.push(songsData[i]);
-            }
-            console.log(songs);
-            callback(songsData);
-        });
-        // load more songs to moreSongs array
-        var moreSongLoader = new XMLHttpRequest();
-        moreSongLoader.open("GET", "more-songs.json");
-        moreSongLoader.send();
-        moreSongLoader.addEventListener("error", function(event) {
-            console.log('There was an error loading more-songs.json');
-        });
-        moreSongLoader.addEventListener("load", function(event) {
-            var moreSongsData = JSON.parse(event.target.responseText);
-            for (var i in moreSongsData) {
-                moreSongs.push(moreSongsData[i]);
-            }
+        $.ajax({
+            url:"songs.json"
+        }).done(function(result){
+            $.each(result, function(iterator) {
+                songs.push(result[iterator]);
+            });
+            SongList.populateSongs();
         });
 
+        // load more songs to moreSongs array
+        $.ajax({
+            url:"more-songs.json"
+        }).done(function(result){
+            $.each(result, function(iterator) {
+                moreSongs.push(result[iterator]);
+            });
+        });
     };
 
     songPopulator.populateSongs = function() {
+        // console.log('populateSongs initiated');
         for (var i = 0; i < songs.length; i++) {
             var songsSong = songs[i];
-            mainContent.innerHTML += `  <div class='song'>
+            $("#selections").append(`<div class='song'>
                                             <h2>${songsSong.song}</h2>
                                             <p>${songsSong.artist}</p>
                                             <p>${songsSong.album}</p>
                                             <p>${songsSong.genre}</p>
                                             <button type="button" class="remove-button">Remove</button>
-                                        </div>`;
+                                        </div>`);
         }
-        mainContent.innerHTML += `<button type="button" id="more-button" >More ></button>`;
+        $("#selections").append(`<button type="button" id="more-button" >More ></button>`);
+        // Clicking on more button shows more songs listener
+        $("#more-button").click(SongList.populateMoreSongs);
+        // Clicking on remove button removes song listener
+        $(".remove-button").click(function(){
+            $(this).closest("div").remove();
+        });
     };
 
     songPopulator.populateMoreSongs = function() {
-        var theMoreButton = mainContent.lastElementChild;
-        mainContent.removeChild(theMoreButton);
+        $("#more-button").remove();
         for (var i = 0; i < moreSongs.length; i++) {
             var songsSong = moreSongs[i];
-            mainContent.innerHTML +=  ` <div class='song'>
+            $("#selections").append(` <div class='song'>
                                             <h2>${songsSong.song}</h2>
                                             <p>${songsSong.artist}</p>
                                             <p>${songsSong.album}</p>
                                             <p>${songsSong.genre}</p>
                                             <button type="button" class="remove-button">Remove</button>
-                                        </div>`;
+                                        </div>`);
         }
-        mainContent.innerHTML += `<button type="button" id="more-button" >More ></button>`;
+        $("#selections").append(`<button type="button" id="more-button" >More ></button>`);
+        // Clicking on more button shows more songs listener
+        $("#more-button").click(SongList.populateMoreSongs);
+        // Clicking on remove button removes song listener
+        $(".remove-button").click(function(){
+            $(this).closest("div").remove();
+        });
     };
 
-// Hide and show divs based on clicking "Add Music"
-$("#addMusic").click(showAdd);
+    // Hide and show divs based on clicking "Add Music" listener
+    $("#addMusic").click(showAdd);
 
-function showAdd() {
-    $("#addMusicView").show();
-    $("#listMusicView").hide();
-}
+    function showAdd() {
+        console.log('you clicked Add Music');
+        $("#addMusic").addClass("selected");
+        $("#listMusic").removeClass("selected");
+        $("#addMusicView").show();
+        $("#listMusicView").hide();
+    }
 
+    $("#listMusic").click(showList);
+
+    function showList() {
+        console.log('you clicked List Music');
+        $("#addMusic").removeClass("selected");
+        $("#listMusic").addClass("selected");
+        $("#addMusicView").hide();
+        $("#listMusicView").show();
+    }
 
 // // Once the user fills out the song form and clicks the add button, you should collect all values from the input fields, add the song to your array of songs
 $("#addButton").click(addSongToSongs);
@@ -86,10 +98,14 @@ function addSongToSongs() {
     songToAdd.album = $("#addAlbumName").val();
     // add the new song object to the array of songs
     songs.push(songToAdd);
-    // clear out the selections div
-    $("#selections").html("");
-    // repopulate the selections div with the array that includes the new song
-    songPopulator.populateSongs();
+    // populate song to top of "selections" div
+    $("#selections").prepend(`<div class='song'>
+                                <h2>${songToAdd.song}</h2>
+                                <p>${songToAdd.artist}</p>
+                                <p>${songToAdd.album}</p>
+                                <p>${songToAdd.genre}</p>
+                                <button type="button" class="remove-button">Remove</button>
+                            </div>`);
     // clear the input fields
     $("#addSongName").val("");
     $("#addArtistName").val("");
@@ -97,10 +113,14 @@ function addSongToSongs() {
     // switch to the "List Music view"
     $("#addMusicView").hide();
     $("#listMusicView").show();
+    // change the styling on the links
+    $("#addMusic").removeClass("selected");
+    $("#listMusic").addClass("selected");
 }
     return songPopulator;
 })(SongList || {});
-SongList.loadSongs(SongList.populateSongs);
+SongList.loadSongs();
+
 
 
 
